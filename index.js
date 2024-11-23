e11c3c3baf380d29d8f2e2ddd82e5ed509cc8299
Fernando Gustavo <fernando.gdev@gmail.com>
2024-11-23 00:15:29 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import express from 'express'
+import cors from 'cors'
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express();
 const PORT = 3000;
 
 // Middleware
-app.use(bodyParser.json());
+app.use(express.json())
 app.use(cors());
+
+
+const cursos = ['NodeJS', 'React Native', 'React JS']
 
 // Rota inicial
 app.get('/', (req, res) => {
   res.send({ message: 'API funcionando!' });
 });
+
+
+// Listar cursos
+app.get('/cursos', (req, res) => {
+  return res.json(cursos);
+});
+
+
+//exemplo 2 lista todos os herois
+app.get(('/lista'), async (req, res)=>{
+
+  const hero = await prisma.heros.findMany()
+
+  res.status(200).json(hero)
+})
+
 
 // Inicia o servidor
 app.listen(PORT, () => {
